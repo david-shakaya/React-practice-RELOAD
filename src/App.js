169a929d79911f888ab517962b398b10a99564c7
Давид Шакая <66774063+david-shakaya@users.noreply.github.com>
2021-06-09 22:41:13 +0300

    add file

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+
 const link = "google";
 
 function App() {
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+// import Painting from "./components/Painting/js";
 
 ReactDOM.render(
   <React.StrictMode>
